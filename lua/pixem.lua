ff0e484e87cf3355aed6fc486d15736d622e0a24
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2024-12-26 13:20:23 +0100

    Fallback to the part under cursor (<cexpr>) when mixed units are detected

--- lua/pixem.lua
+++ lua/pixem.lua
@@ -3,49 +3,71 @@ local M = {}
 ---@class pixem.Config
 ---@field root_font_size number Font size for calculating conversions
 ---@field use_rem boolean Convert px to rem. Set to false to convert px to em
+
+---@alias pixem.Unit "px"|"em"|"rem"
+
+---@type pixem.Config
 local config = {
     root_font_size = 16,
     use_rem = true
 }
 
----Convert px and em
----@param str string
 ---@param opts pixem.Config
----@return string | nil
-local function convert(str, opts)
-    local substitutions = {
+---@return table
+local function get_substitutions(opts)
+    return {
         em = function(size) return size * opts.root_font_size .. "px" end,
         rem = function(size) return size * opts.root_font_size .. "px" end,
         px = function(size)
             return size / opts.root_font_size .. (opts.use_rem and "rem" or "em")
         end,
     }
+end
 
-    local found_unit = nil
+---@param str any
+---@param opts pixem.Config
+---@return pixem.Unit|nil
+local function find_unit(str, opts)
+    local substitutions = get_substitutions(opts)
+    local found = nil
     for unit, _ in pairs(substitutions) do
         local pattern = "(%d+%.?%d*)" .. unit
         if str:find(pattern) then
-            if found_unit then
-                print("Pixem: mixed units detected")
-                return nil
+            if not found then
+                found = unit
             else
-                found_unit = unit
+                return nil
             end
         end
     end
+    return found
+end
 
-    local pattern = "(%d+%.?%d*)" .. found_unit
-    local converted = str:gsub(pattern, substitutions[found_unit])
+---Convert px and em
+---@param str string
+---@param unit pixem.Unit
+---@param opts pixem.Config
+---@return string
+local function convert(str, unit, opts)
+    local substitutions = get_substitutions(opts)
+
+    local pattern = "(%d+%.?%d*)" .. unit
+    local converted = str:gsub(pattern, substitutions[unit])
     return converted
 end
 
----Execute pixem on the current line
+---Execute pixem on the current line, fallback to current word
 ---@param opts? pixem.Config
 M.run_line = function(opts)
     opts = vim.tbl_extend("keep", opts or {}, config)
 
     local line = vim.api.nvim_get_current_line()
-    local new_line = convert(line, opts)
+    local unit = find_unit(line, opts) or find_unit(vim.fn.expand("<cexpr>"), opts)
+    if not unit then
+        print("Pixem: mixed units detected. Place the cursor on the specific part to be converted and try again.")
+        return
+    end
+    local new_line = convert(line, unit, opts)
     if new_line then
         vim.api.nvim_set_current_line(new_line)
     end
@@ -62,6 +84,9 @@ M.setup = function(opts)
     end, {})
 end
 
+vim.print(vim.fn.expand("<cWord>"))
+
+M._find_unit = find_unit
 M._convert = convert
 
 return M
--- tests/pixem_spec.lua
+++ tests/pixem_spec.lua
@@ -1,53 +1,82 @@
-describe("pixem", function()
+local opts = {
+    root_font_size = 16,
+    use_rem = true
+}
+
+describe("pixem.find_unit", function()
+    local find_unit = require("pixem")._find_unit
+
+    it("px", function()
+        local unit = find_unit("16px", opts)
+        assert.equals(unit, "px")
+    end)
+
+    it("em", function()
+        local unit = find_unit("1em", opts)
+        assert.equals(unit, "em")
+    end)
+
+    it("rem", function()
+        local unit = find_unit("1rem", opts)
+        assert.equals(unit, "rem")
+    end)
+
+    it("no unit -> nil", function()
+        local unit = find_unit("no unit string", opts)
+        assert.equals(unit, nil)
+    end)
+end)
+
+describe("pixem.convert", function()
     local convert = require("pixem")._convert
 
-    local opts = {
-        root_font_size = 16,
-        use_rem = true
-    }
-
     it("px -> rem", function()
-        local converted = convert("16px", opts)
+        local converted = convert("16px", "px", opts)
         assert.equals(converted, "1rem")
     end)
 
     it("px -> em", function()
-        local converted = convert("16px", vim.tbl_extend("force", opts, { use_rem = false }))
+        local converted = convert("16px", "px", vim.tbl_extend("force", opts, { use_rem = false }))
         assert.equals(converted, "1em")
     end)
 
     it("em -> px", function()
-        local converted = convert("1em", opts)
+        local converted = convert("1em", "em", opts)
         assert.equals(converted, "16px")
     end)
 
     it("converts in the middle of a string", function()
-        local converted = convert("some string 16px string continues", opts)
+        local converted = convert("some string 16px string continues", "px", opts)
         assert.equals(converted, "some string 1rem string continues")
     end)
 
     it("takes root_font_size into account px -> rem", function()
-        local converted = convert("16px", vim.tbl_extend("force", opts, { root_font_size = 10 }))
+        local converted = convert("16px", "px", vim.tbl_extend("force", opts, { root_font_size = 10 }))
         assert.equals(converted, "1.6rem")
     end)
 
     it("takes root_font_size into account rem -> px", function()
-        local converted = convert("1rem", vim.tbl_extend("force", opts, { root_font_size = 10 }))
+        local converted = convert("1rem", "rem", vim.tbl_extend("force", opts, { root_font_size = 10 }))
         assert.equals(converted, "10px")
     end)
 
     it("multiple occurences rem -> px", function()
-        local converted = convert("1rem 2rem 3rem 4rem", opts)
+        local converted = convert("1rem 2rem 3rem 4rem", "rem", opts)
         assert.equals(converted, "16px 32px 48px 64px")
     end)
 
     it("multiple occurences px -> rem", function()
-        local converted = convert("16px 32px 48px 64px", opts)
+        local converted = convert("16px 32px 48px 64px", "px", opts)
         assert.equals(converted, "1rem 2rem 3rem 4rem")
     end)
 
-    it("returns nil for mixed units", function()
-        local converted = convert("16px 2rem 3em", opts)
-        assert.equals(converted, nil)
+    it("mixed units", function()
+        local converted = convert("16px 2rem 3em", "px", opts)
+        assert.equals(converted, "1rem 2rem 3em")
+    end)
+
+    it("Inexistant unit", function()
+        local converted = convert("16px 12px 24px", "em", opts)
+        assert.equals(converted, "16px 12px 24px")
     end)
 end)
